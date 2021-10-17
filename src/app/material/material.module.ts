0764380f3f26626c7c2cb2063353cda89c3a6e8e
kevinstif <kevin_stif_2012@hotmail.com>
2021-10-17 15:14:35 -0500

    styles of addPost
    cambio de estilo de la pantalla de crear publicacion

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
@@ -22,6 +23,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
   ],
   exports:[
     MatCardModule,
+    MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatDividerModule,
