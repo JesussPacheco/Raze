4f267112292237987de5bf0bc473dd8ee425c6af
Jesús Emerson Pacheco Alanya <jesuspacheco0219@gmail.com>
2021-10-22 01:57:53 -0500

    window emergent connect
    connect components with management-outfit types

--- src/app/components/outfit-types/pages/outfit-types-delete/outfit-types-delete.component.ts
+++ src/app/components/outfit-types/pages/outfit-types-delete/outfit-types-delete.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import { Inject } from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
 @Component({
   selector: 'app-outfit-types-delete',
   templateUrl: './outfit-types-delete.component.html',
   styleUrls: ['./outfit-types-delete.component.css']
 })
 export class OutfitTypesDeleteComponent implements OnInit {
-
-  constructor() { }
+  pepito=3;
+  constructor(public dialog: MatDialog,@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    console.log(this.pepito)
+    console.log(this.data.id)
   }
 
 }
--- src/app/components/outfit-types/pages/outfit-types-management/outfit-types-management.component.ts
+++ src/app/components/outfit-types/pages/outfit-types-management/outfit-types-management.component.ts
@@ -3,6 +3,8 @@ import {OutfitType} from "../../model/outfit-type";
 import {OutfitTypesService} from "../../services/outfit-types.service";
 import {MatDialog} from "@angular/material/dialog";
 import {OutfitTypeItemComponent} from "../outfit-type-item/outfit-type-item.component";
+import {OutfitTypesEditComponent} from "../outfit-types-edit/outfit-types-edit.component";
+import {OutfitTypesDeleteComponent} from "../outfit-types-delete/outfit-types-delete.component";
 
 @Component({
   selector: 'app-outfit-types-management',
@@ -21,9 +23,25 @@ export class OutfitTypesManagementComponent implements OnInit {
   ngOnInit(): void {
     this.retrieveOutfitsTypes();
   }
-  openDialog() {
+  openDialogMore(id:number) {
     const dialogRef = this.dialog.open(OutfitTypeItemComponent);
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  openDialogEdit(id:number){
+    const dialogRef = this.dialog.open(OutfitTypesEditComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  openDialogDelete(id:number){
+    const dialogRef = this.dialog.open(OutfitTypesDeleteComponent,{
+      data: {
+        id:id
+      }
+    });
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
